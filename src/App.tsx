0838a3035d2b045793e0a3c6271ebb6046bd9495
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-09 20:11:21 +0300

    wish list lesson 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
-import {WishList} from './WishList';
+import {OsTypeForSelect, WishList} from './WishList';
 import {v1} from 'uuid';
 
-export type OSType = 'iOS' | 'Android' | 'All'
+
+type OsType = 'All' | OsTypeForSelect
 export type WishesType = {
     id: string
     title: string
-    OS: OSType
+    OS: OsType
     checked: boolean
 }
 
@@ -20,13 +21,22 @@ function App() {
         {id: v1(), title: 'Xiaomi 13', OS: 'Android', checked: false},
         {id: v1(), title: 'Huawei', OS: 'Android', checked: false}
     ])
-    const addWish = (newItem: string, newOs: OSType) => {
-        const newWish: WishesType = {id: v1(), title: newItem, OS: newOs, checked: false}
-        setWishes([newWish, ...wishes])
+    const [newTitle, setNewTitle] = useState<string>('')
+
+    const addWish = (os: OsTypeForSelect) => {
+        setWishes([{id: v1(), title: newTitle, OS: os, checked: false}, ...wishes])
     }
+
+    // const addWish = (newItem: string, newOs: OSType) => {
+    //     const newWish: WishesType = {id: v1(), title: newItem, OS: newOs, checked: false}
+    //     setWishes([newWish, ...wishes])
+    // }
+
+    const removeWish = (id: string) => setWishes(wishes.filter(el => el.id !== id))
+
     return (
         <div className="App">
-            <WishList wishes={wishes} addWish={addWish}/>
+            <WishList wishes={wishes} setNewTitle={setNewTitle} newTitle={newTitle} addWish={addWish} removeWish={removeWish}/>
         </div>
     );
 }
--- src/WishList.tsx
+++ src/WishList.tsx
@@ -1,35 +1,63 @@
-import React, {ChangeEvent, useState} from 'react';
-import {OSType, WishesType} from './App';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {WishesType} from './App';
 
 type WishListPropsType = {
     wishes: WishesType[]
-    addWish: (newItem: string, newOs: OSType) => void
+    // addWish: (newItem: string, newOs: OSType) => void
+    setNewTitle: (newTitle: string) => void
+    newTitle: string
+    addWish: (os: OsTypeForSelect) => void
+    removeWish: (id: string) => void
 }
+export type OsTypeForSelect = 'Select OS' | 'Android' | 'iOS'
 
 export const WishList = (props: WishListPropsType) => {
-    const [newTitle, setNewTitle] = useState<string>('')
-    const [newOs, setNewOs] = useState<OSType>('All')
-
-    const addWishTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setNewTitle(e.currentTarget.value)
-    const addNewItemHandler = () => {
-        props.addWish(newTitle, newOs)
-        setNewTitle('')
-        setNewOs('All')
+    // const [newTitle, setNewTitle] = useState<string>('')
+    const [os, setOs] = useState<OsTypeForSelect>('Select OS')
+    //
+    // const addWishTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setNewTitle(e.currentTarget.value)
+    // const addNewItemHandler = () => {
+    //     props.addWish(newTitle, newOs)
+    //     setNewTitle('')
+    //     setNewOs('All')
+    // }
+    // const changeOSHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    //     setNewOs(e.currentTarget.value as OSType)
+    // }
+    const [error, setError] = useState<boolean>(false)
+    const onSetNewWishTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        error && setError(false)
+        props.setNewTitle(e.currentTarget.value)
     }
-    const changeOSHandler = (e: ChangeEvent<HTMLSelectElement>) => {
-        setNewOs(e.currentTarget.value as OSType)
+    const addWishHandler = () => {
+        if (os !== 'Select OS' && props.newTitle.trim() !== '') {
+            props.addWish(os)
+            props.setNewTitle('')
+            setOs('Select OS')
+        } else {
+            setError(true)
+        }
     }
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && addWishHandler()
+    const onChangeOsHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        error && setError(false)
+        setOs(e.currentTarget.value as OsTypeForSelect)
+    }
+    const removeWishHandler = (id: string) => props.removeWish(id)
     return (
         <div>
+            <h1>Phones</h1>
             <div>
-                <input placeholder="Enter an item" value={newTitle} onChange={addWishTitleHandler}/>
-                <select value={newOs} onChange={changeOSHandler}>
-                    <option selected value="All">Select OS</option>
+                <input placeholder="Enter an title" value={props.newTitle} onChange={onSetNewWishTitleHandler}
+                       onKeyDown={onKeyDownHandler}/>
+                <select value={os} onChange={onChangeOsHandler}>
+                    <option value="Select OS">Select OS</option>
                     <option value="Android">Android</option>
                     <option value="iOS">iOS</option>
                 </select>
-                <button onClick={addNewItemHandler}>Add</button>
+                <button onClick={addWishHandler}>Add</button>
             </div>
+            {error && <p style={{color: 'red'}}>Fields title and os are required!</p>}
             <ul>
                 {props.wishes.map(el => {
                     return (
@@ -38,7 +66,7 @@ export const WishList = (props: WishListPropsType) => {
                             <span>{el.title}</span>
                             <span> / OS: </span>
                             <span>{el.OS}</span>
-                            <button>x</button>
+                            <button onClick={() => removeWishHandler(el.id)}>x</button>
                         </li>
                     )
                 })}
