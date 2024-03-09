4fef7d336570e95898ab087aa9f123a6fbf1cdb6
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-09 13:49:38 +0300

    start wish list lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React, {useState} from 'react';
 import './App.css';
 import {WishList} from './WishList';
+import {v1} from 'uuid';
 
-export type OSType = 'iOS' | 'Android' | '-'
+export type OSType = 'iOS' | 'Android' | 'All'
 export type WishesType = {
-    id: number
+    id: string
     title: string
     OS: OSType
-    price: number
-    category: 'Phones'
     checked: boolean
 }
 
 function App() {
     const [wishes, setWishes] = useState<WishesType[]>([
-        {id: 1, title: 'IPhone 13 ProMax', OS: 'iOS', price: 1200, category: 'Phones', checked: false},
-        {id: 2, title: 'IPhone 14', OS: 'iOS', price: 1400, category: 'Phones', checked: false},
-        {id: 3, title: 'Samsung Galaxy Fold 4', OS: 'Android', price: 1500, category: 'Phones', checked: false},
-        {id: 4, title: 'Samsung Galaxy S23', OS: 'Android', price: 1300, category: 'Phones', checked: false},
-        {id: 5, title: 'Xiaomi 13', OS: 'Android', price: 1000, category: 'Phones', checked: false},
-        {id: 6, title: 'Huawei', OS: 'Android', price: 900, category: 'Phones', checked: false},
-        {id: 7, title: 'Nokia 3310', OS: '-', price: 100, category: 'Phones', checked: false}
+        {id: v1(), title: 'IPhone 13 ProMax', OS: 'iOS', checked: true},
+        {id: v1(), title: 'IPhone 14', OS: 'iOS', checked: true},
+        {id: v1(), title: 'Samsung Galaxy Fold 4', OS: 'Android', checked: true},
+        {id: v1(), title: 'Samsung Galaxy S23', OS: 'Android', checked: false},
+        {id: v1(), title: 'Xiaomi 13', OS: 'Android', checked: false},
+        {id: v1(), title: 'Huawei', OS: 'Android', checked: false}
     ])
     return (
         <div className="App">
--- src/WishList.tsx
+++ src/WishList.tsx
@@ -8,18 +8,38 @@ type WishListPropsType = {
 export const WishList = (props: WishListPropsType) => {
     return (
         <div>
-            <h1>{props.wishes[0].category}</h1>
+            <div>
+                <input placeholder='Enter an item'/>
+                <select>
+                    <option value="All">Select OS</option>
+                    <option value="Android">Android</option>
+                    <option value="iOS">iOS</option>
+                </select>
+                <button>Add</button>
+            </div>
             <ul>
                 {props.wishes.map(el => {
                     return (
                         <li key={el.id}>
                             <input type="checkbox" checked={el.checked}/>
-                            <span>{el.title}</span>{` - ${el.OS} - ${el.price} `}
+                            <span>{el.title}</span>
+                            <span> / OS: </span>
+                            <span>{el.OS}</span>
                             <button>x</button>
                         </li>
                     )
                 })}
             </ul>
+            <div>
+                FILTER BY:
+                <div>
+                    <select>
+                        <option value="All">All</option>
+                        <option value="Android">Android</option>
+                        <option value="iOS">iOS</option>
+                    </select>
+                </div>
+            </div>
         </div>
     );
 };
